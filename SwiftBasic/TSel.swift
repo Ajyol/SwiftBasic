3593b07d938a55d02ad38c8da5652ab0ea1418a8
Ajyol <ajyold42@gmail.com>
2024-09-18 16:29:55 -0500

    Update TSel.swift

--- SwiftBasic/TSel.swift
+++ SwiftBasic/TSel.swift
@@ -1,10 +1,19 @@
-//
-//  TSel.swift
-//  SwiftBasic
-//
-//  Created by Ajyol Dhamala on 9/18/24.
-//
-
-func TSel(){
+func TSel() {
+    print("Enter your score to check if it is greater than 40: ", terminator: "")
     
+    if let userInput = readLine(), let teamScore = Int(userInput) {
+
+        let scoreDecoration: String
+        if teamScore > 40 {
+            scoreDecoration = "🎉"
+        } else {
+            scoreDecoration = "👎"
+        }
+        
+        // Print the result
+        print("Score: \(teamScore) \(scoreDecoration)")
+    } else {
+        // Handle invalid input
+        print("Invalid input. Please enter a valid integer.")
+    }
 }
